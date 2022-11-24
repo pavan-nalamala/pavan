eb4e643169f7743665f52008a8984e9984505761
pavan-nalamala <118965083+pavan-nalamala@users.noreply.github.com>
2022-11-24 13:41:12 +0530

    Add files via upload

---
+++ javascriptobjects.js
@@ -0,0 +1,123 @@
+  /*  window.pavan = 5;
+    window.pp1 = 6;
+    window.pp3 = 10;
+
+function pp7(name,age,job)
+{
+    this.name = name;
+    this.age=age;
+    this.job=job;
+}
+var chintu = new pp7('pavan',24,'software');
+console.log(chintu);
+*/
+/*function pp5()
+{
+    open("");
+    document.close();
+}
+*/
+/*
+$('.select-language').on('change', function(){
+    location.href = $(this).val();
+});
+*/
+/*function pp2()
+{
+    document.getElementById('pp1').innerHTML="hello welcome pavan";
+    
+}
+var pp5 = prompt("what is ur name?");
+if(pp5 !== null){alert("welcome "  +pp5)};
+document.write("Learn and come again with new");
+*/
+/*document.getElementById('pp1');
+pp4.getAttribute('id');
+pp4.setAttribute('id','king');
+alert(pp4.getAttribute('id'));
+*/
+/*function myFunction() {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode("Water");
+    node.appendChild(textnode);
+    document.getElementById("myList").appendChild(node);
+    }
+*/
+$(document).ready(function()
+{
+    $('puchki').
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
